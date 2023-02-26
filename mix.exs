9ee740c292ea36fa5d951a8535878c425f1a72d1
debora-be <arobedbee@gmail.com>
2023-02-26 16:57:16 -0300

    fix: version value

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirInterviewStarter.MixProject do
   def project do
     [
       app: :elixir_interview_starter,
-      version: "0.1.1",
+      version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
