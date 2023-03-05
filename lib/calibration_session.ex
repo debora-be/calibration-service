c0f188891d8b9cb90aa453b3277794465a6bc1e4
debora-be <arobedbee@gmail.com>
2023-03-05 18:44:54 -0300

    update: fields for extra validations in the schema

--- lib/calibration_session.ex
+++ lib/calibration_session.ex
@@ -4,15 +4,17 @@ defmodule ElixirInterviewStarter.CalibrationSession do
   belongs to, what step the session is on, and any other information relevant to working
   with the session.
   """
-  use GenServer
 
   @type t() :: %__MODULE__{
           user_email: String.t(),
           session_id: String.t(),
           user_has_ongoing_calibration_session: boolean,
+          precheck_1_message: String.t(),
           precheck_1_succeeded: boolean,
           user_has_ongoing_calibration_session_that_just_finished_precheck1: boolean,
+          precheck_2_message: String.t(),
           precheck_2_succeeded: boolean,
+          calibration_message: String.t(),
           calibration_succeeded: boolean
         }
 
@@ -20,9 +22,12 @@ defmodule ElixirInterviewStarter.CalibrationSession do
     :user_email,
     :session_id,
     :user_has_ongoing_calibration_session,
+    :precheck_1_message,
     :precheck_1_succeeded,
     :user_has_ongoing_calibration_session_that_just_finished_precheck1,
+    :precheck_2_message,
     :precheck_2_succeeded,
+    :calibration_message,
     :calibration_succeeded
   ]
 end
