b27eb6f0d13ff4feae4f136456a148bbd62dd6e1
debora-be <arobedbee@gmail.com>
2023-03-05 18:47:57 -0300

    refactor: splits the business logic into smaller modules

---
+++ lib/operational/start_precheck_1.ex
@@ -0,0 +1,41 @@
+defmodule ElixirInterviewStarter.Operational.StartPrecheck1 do
+  @moduledoc """
+  Starts the precheck_1 step of the ongoing `CalibrationSession` for the provided user.
+  """
+
+  alias ElixirInterviewStarter.CalibrationSession
+
+  alias ElixirInterviewStarter.CalibrationSessions.CreateCurrentUserSession
+  alias ElixirInterviewStarter.DeviceMessages
+
+  alias ElixirInterviewStarter.Operational.{CalibrationSessionManager, Server}
+
+  @type user_email :: String.t()
+
+  @precheck_1_message "startPrecheck1"
+
+  @spec process(user_email) :: {:ok, %CalibrationSession{}} | map()
+  @doc """
+  Starts the precheck_1 step of the ongoing `CalibrationSession` for the provided user.
+  If the user has already an ongoing `CalibrationSession`, the `CalibrationSession` cannot continue.
+  """
+  def process(user_email) do
+    with {:ok, pid} = Server.start_link(%CalibrationSession{}),
+         false <- CalibrationSessionManager.user_has_ongoing_calibration_session?(user_email) do
+      DeviceMessages.send(user_email, @precheck_1_message)
+
+      initial_calibration_session =
+        CreateCurrentUserSession.process(%{
+          user_email: user_email,
+          session_id: pid,
+          user_has_ongoing_calibration_session: false,
+          precheck_1_message: @precheck_1_message,
+          precheck_1_succeeded: true
+        })
+
+      {:ok, initial_calibration_session}
+    else
+      _ -> %{"precheck1" => false}
+    end
+  end
+end
